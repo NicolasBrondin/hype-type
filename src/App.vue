14e4f43b02d77066c8027f44e55d4ccab5527945
Nicolas Brondin-Bernard <nicolas@brondin.com>
2020-11-08 19:30:36 +0100

    [FEATURE] Car - Added main components of the game

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/off-road.svg" height="300">
+    <h1>Sélectionne ta voiture</h1>
+    <div style="display: flex;align-items: center;width: 100%;justify-content: center;">
+      <button class="arrow-button">
+        <img src="./assets/arrow.svg" height="50" style="transform: rotate(180deg);"/>
+      </button>
+      <img class="car" alt="Voiture" style="" src="./assets/off-road.svg" />
+      <button class="arrow-button">
+        <img src="./assets/arrow.svg" height="50"/>
+        </button>
+    </div>
+    <SpeechBubble/>
   </div>
 </template>
 
 <script>
+import SpeechBubble from './components/SpeechBubble.vue'
 
 export default {
   name: 'App',
   components: {
+    SpeechBubble
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
+  align-items: center;
+}
+h1 {
+  margin-bottom: 20px;
+  font-family: Roboto;
+}
+.arrow-button {
+  background: transparent;
+  border: 0;
+  outline: 0;
+  opacity: 0.1;
+}
+.car {
+  width: 50%;
+  max-width: 300px;
+  margin: 0px 40px;
 }
 </style>
---
+++ src/components/SpeechBubble.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="bubble-container">
+        Je suis la vieille voiture de ton oncle !
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .bubble-container {
+        position: relative;
+        display: inline-block;
+        width: 400px;
+        padding: 10px;
+        border-radius: 10px;
+        box-shadow: 0px 0px 10px 5px rgba(0,0,0,0.1);
+        margin-top: 40px;
+        background: #3498db;
+        color: white;
+        text-transform: uppercase;
+        font-weight: bold;
+        font-family: Roboto;
+    }
+
+    .bubble-container::before {
+        position: absolute;
+        content: "";
+        top: -10px;
+        left: calc(50% + 60px);
+        width: 0;
+        height: 0;
+        border-style: solid;
+        border-width: 0 10px 10px 10px;
+        border-color: transparent transparent #3498db transparent;
+    }
+</style>
