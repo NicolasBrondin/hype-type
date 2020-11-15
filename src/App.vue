eb760f6ab897b96bef4a7dcfd97425f684efe299
Nicolas Brondin-Bernard <nicolas@brondin.com>
2020-11-15 19:39:04 +0100

    [UI/UX] Game - Stylized the UI and added multiple sentences support

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="app" v-if="word">
-        <input type="text" v-model="user_text" @keyup="refresh_text" />
-        <p>{{user_text}}<span style="color: grey;">{{to_be_written}}</span></p>
+    <div id="app">
+      <div style="width: 100%;position: relative;">
+        <h1>Tape ce texte le plus vite possible</h1>
+        <input id="main-input" type="text" v-model="user_text" @keyup="refresh_text" />
+        <p>{{user_text}}<span>{{to_be_written}}</span></p> 
+      </div>   
     </div>
 </template>
 
@@ -12,31 +15,49 @@ export default {
   data: function(){
     return {
       sentences: [
-        "je dois taper ce texte"
+        "je dois taper ce texte",
+        "et celui là aussi"
       ],
       word: null,
+      sentence_index: 0,
       word_index: 0,
       user_text: "",
       
     }
   },
   mounted: function(){
-    this.word = this.sentences[0].split(' ')[this.word_index];
+    this.word = this.sentences[this.sentence_index].split(' ')[this.word_index];
+    document.querySelector("#main-input").focus();
+    document.querySelector("#main-input").addEventListener("blur", function(){
+      setTimeout(function(){document.querySelector("#main-input").focus()}, 0);
+    });
   },
   computed: {
     to_be_written: function(){
-      return this.word.substr(this.user_text.length);
+      if(this.word){
+        return this.word.substr(this.user_text.length);
+      } else {
+        return "";
+      }
     }
   },
   methods: {
     refresh_text: function(){
-      console.log(this.word.length);
-      console.log(this.user_text.length);
       if(this.word.indexOf(this.user_text) !== 0) {
         this.user_text = this.user_text.substr(0,this.user_text.length-1);
       } else if(this.word.indexOf(this.user_text) === 0 && this.word.length === this.user_text.length){
-        this.word = this.sentences[0].split(' ')[++this.word_index];
+        if(this.word_index === this.sentences[this.sentence_index].split(' ').length - 1){
+          if(this.sentence_index === this.sentences.length -1) {
+            alert("fini");
+          } else {
+            this.sentence_index++;
+            this.word_index = 0;
+          }
+        }
+        console.log(this.sentence_index);
+        this.word = this.sentences[this.sentence_index].split(' ')[++this.word_index];
         this.user_text = "";
+        
       }
     }
   },
@@ -63,14 +84,27 @@ body {
   align-items: center;
   padding: 0px 20px;
 }
-h1 {
-  margin-bottom: 20px;
-  font-family: Roboto;
+input {
+  position: absolute;
+  width: 100%;
+  left: 0;
+  text-align: center;
+  z-index: 1;
+  height: 50px;
+  opacity: 0;
 }
-.arrow-button {
-  background: transparent;
-  border: 0;
-  outline: 0;
-  opacity: 0.1;
+p {
+  display: inline-block;
+  position: absolute;
+  z-index: 2;
+  margin:0;
+  font-size: 60px;
+  text-align: center;
+  width: 100%;
+  left: 0;
+}
+
+span {
+  color: rgba(0,0,0,0.2);
 }
 </style>
